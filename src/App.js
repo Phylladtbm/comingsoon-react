8d115d7d7938793d2694b63d419018c96c3fb0e8
PhewPhyll <bm.adithep@gmail.com>
2024-11-15 17:09:27 +0700

    -

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleButtonClick = () => {
+    window.open('https://linktr.ee/barcampsk', '_blank');
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <div className="content">
+          <p><span>BARCAMP SONGKHLA 9</span> WEBSITE</p>
+          <h1>We’re <span>Launching</span> Soon</h1>
+          <div className="button">
+            <button type="button" onClick={handleButtonClick}>
+              FOLLOW US &gt;&gt;&gt;
+            </button>
+          </div>
+        </div>
+        <div className="content-video">
+          <video controls preload="metadata" muted loop playsInline autoPlay src="images/barcamp09_announcement.mp4"
+            poster="images/thumbnail.png">
+            <source src="images/barcamp09_announcement.mp4" type="video/mp4" />
+            <source src="images/barcamp09_announcement.webm" type="video/webm" />
+            <source src="images/barcamp09_announcement.ogv" type="video/ogg" />
+            This browser does not support HTML5
+          </video>
+        </div>
+      </div>
     </div>
   );
 }
